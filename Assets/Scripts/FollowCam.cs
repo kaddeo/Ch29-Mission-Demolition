7c5814ab2d2aeb0f544c46a993bd0cf874b3a3be
kaddeo <kaddeo@scarletmail.rutgers.edu>
2016-12-01 22:34:47 -0500

    Add files via upload

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+    static public FollowCam S;
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool _______________;
+    public GameObject poi;
+    public float camZ;
+	
+	void Awake() {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+	void Update () {
+        if (poi == null) return;
+        Vector3 destination = poi.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;	
+	}
+}
